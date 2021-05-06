280df7f40adb8c4ee7f645a35f563d30afee3cfe
Evgeniy Sevostyanov <exocornet@gmail.com>
2021-05-06 18:22:16 +0300

    мобильное меню

--- src/js/main.js
+++ src/js/main.js
@@ -76,4 +76,9 @@
   });
 
 
+  document.querySelector('.menu-hamburger').onclick = function () {
+    document.querySelector('.menu-hamburger__icon').classList.toggle('menu-hamburger__icon_active');
+    document.querySelector('.menu').classList.toggle('menu_active');
+  }
+
 })(jQuery);
