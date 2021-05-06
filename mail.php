984851cdc9bb7d5ab835f90356c77a3a7c9f3c15
Evgeniy Sevostyanov <exocornet@gmail.com>
2021-05-06 22:30:00 +0300

    С файлом php обратной связи

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php
+$to = "exocornet@gmail.com"; //емайл получателя
+$tema = "Форма обратной связи закза звонка"; //ема
+$message = "Ваше имя: ".$_POST['name']."<br>";//присвоить переменной значение, полученное из формы name=name
+  $message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
+$message .= "Номер телефона: ".$_POST['phone']."<br>"; //полученное из формы name=phone
+$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+header("Location: http://bodycam.videosoft.pro/)
+?>
