f51fd06adcaa4cfc94b5f6b5998f1346717753fa
Evgeniy Sevostyanov <exocornet@gmail.com>
2021-05-08 00:15:04 +0300

    исправление ошибок

--- src/js/main.js
+++ src/js/main.js
@@ -46,6 +46,7 @@
 
     document.querySelector('.menu-hamburger__icon').classList.toggle('menu-hamburger__icon_active');
     document.querySelector('.menu').classList.toggle('menu_active');
+    document.querySelector('.phone').classList.toggle('phone_active');
     document.querySelector('.film').classList.toggle('film_active');
 
     var plansOffset = $('#product').offset().top;
@@ -62,6 +63,7 @@
 
     document.querySelector('.menu-hamburger__icon').classList.toggle('menu-hamburger__icon_active');
     document.querySelector('.menu').classList.toggle('menu_active');
+    document.querySelector('.phone').classList.toggle('phone_active');
     document.querySelector('.film').classList.toggle('film_active');
 
     var plansOffset = $('#demo').offset().top;
@@ -78,6 +80,36 @@
 
     document.querySelector('.menu-hamburger__icon').classList.toggle('menu-hamburger__icon_active');
     document.querySelector('.menu').classList.toggle('menu_active');
+    document.querySelector('.phone').classList.toggle('phone_active');
+    document.querySelector('.film').classList.toggle('film_active');
+
+    var plansOffset = $('#form').offset().top;
+
+    $('html, body').animate({
+      scrollTop: plansOffset
+    }, 1000);
+
+  });
+
+  $('#btn-scroll-demo').on('click', function (e) {
+
+    e.preventDefault();
+
+    var plansOffset = $('#demo').offset().top;
+
+    $('html, body').animate({
+      scrollTop: plansOffset
+    }, 1000);
+
+  });
+
+  $('#form-call').on('click', function (e) {
+
+    e.preventDefault();
+
+    document.querySelector('.menu-hamburger__icon').classList.toggle('menu-hamburger__icon_active');
+    document.querySelector('.menu').classList.toggle('menu_active');
+    document.querySelector('.phone').classList.toggle('phone_active');
     document.querySelector('.film').classList.toggle('film_active');
 
     var plansOffset = $('#form').offset().top;
@@ -92,12 +124,14 @@
   document.querySelector('.menu-hamburger').addEventListener('click', function () {
     document.querySelector('.menu-hamburger__icon').classList.toggle('menu-hamburger__icon_active');
     document.querySelector('.menu').classList.toggle('menu_active');
+    document.querySelector('.phone').classList.toggle('phone_active');
     document.querySelector('.film').classList.toggle('film_active');
   });
 
   document.querySelector('.film').addEventListener('click', function () {
     document.querySelector('.menu-hamburger__icon').classList.toggle('menu-hamburger__icon_active');
     document.querySelector('.menu').classList.toggle('menu_active');
+    document.querySelector('.phone').classList.toggle('phone_active');
     document.querySelector('.film').classList.toggle('film_active');
   });
 
